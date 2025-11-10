0f685801da3ac5c3a1f2024f9107ecf7c676e3cd
khelmka25 <kihelmka@gmail.com>
2025-11-10 17:33:33 -0600

    Update testing

--- tests/test_chain.cc
+++ tests/test_chain.cc
@@ -1,18 +1,29 @@
 
+#include "ChainArgParser.h"
+
 #include <catch2/catch_test_macros.hpp>
 
-static int Factorial( int number ) {
-   return number <= 1 ? number : Factorial( number - 1 ) * number;  // fail
-// return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // pass
-}
+TEST_CASE("Chain Test") {
 
-TEST_CASE( "2: Factorial of 0 is 1 (fail)", "[multi-file:2]" ) {
-    REQUIRE( Factorial(0) == 1 );
-}
+    int argc = 5;
+    const char* argv[] = {
+        "/build/a.exe",
+        "--add",
+        "8",
+        "-4",
+        "--2"
+    };
 
-TEST_CASE( "2: Factorials of 1 and higher are computed (pass)", "[multi-file:2]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+    ChainArgParser parser;
+    parser.registerHandler({ "-a", "--add", "--sum" }, [](std::vector<Argument>& args) {
+        int sum = 0;
+        for (auto& arg : args) {
+            sum += std::stoi(arg.value);
+        }
+
+        REQUIRE(sum == 6);
+    }, OptionType::kOptional);
+
+    parser.parse(argc, (char**)argv);
+
 }
--- tests/test_index.cc
+++ tests/test_index.cc
@@ -1,4 +1,29 @@
 
-int main() {
-    return 0;
+#include "IndexableArgParser.h"
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Index Test") {
+
+    int argc = 5;
+    const char* argv[] = {
+        "/build/a.exe",
+        "--add",
+        "8",
+        "-4",
+        "--2"
+    };
+
+    IndexibleArgParser parser;
+    parser.parse(argc, (char**)argv);
+
+    if (auto& args = parser[{"-a", "--add", "--sum"}]; !args.empty()) {
+        int sum = 0;
+        for (auto& arg : args) {
+            int v = std::stoi(arg.value);
+            sum += v;
+        }
+        
+        REQUIRE(sum == 6);
+    }
 }
